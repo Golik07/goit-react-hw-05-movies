d971e5881e6cc02e099eecd0f3df84dcfbe8287e
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-10-05 19:09:42 +0200

    update

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -21,8 +21,8 @@ export const StyledLink = styled(NavLink)`
   padding: 8px 16px;
   border-radius: 4px;
   text-decoration: none;
-  color: black;
   font-weight: 500;
+  color: black;
 
   &.active {
     color: white;
